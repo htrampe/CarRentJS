c2719a200a20917aacae7b7b3617857f442f3d37
Holger Trampe <htrampe@gmail.com>
2022-06-29 11:34:31 +0200

    Init

---
+++ src/main/java/CarsRent/CarsRentApplication.java
@@ -0,0 +1,13 @@
+package CarsRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CarsRent/CarsRentApplicationTests.java
@@ -0,0 +1,13 @@
+package CarsRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
